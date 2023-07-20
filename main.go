de53ef10532a3bc208de0af5f3432f03476fbbbd
Keita Sakurai <keitasakuraims@gmail.com>
2023-07-20 22:18:03 +0900

    changed function order

--- main.go
+++ main.go
@@ -12,23 +12,26 @@ type User struct {
 	Age       int    `json:"age"`
 }
 
+func decodeHandler(w http.ResponseWriter, r *http.Request) {
+	var user User                         // structで定義されたUser型の変数user
+	json.NewDecoder(r.Body).Decode(&user) // JSONデータを読み込み、decodeして構造体Userのuserのアドレスに格納
+
+	fmt.Fprintf(w, "%s %s is %d years old!", user.Firstname, user.Lastname, user.Age) // %sは文字列、%dは整数
+}
+
+func encodeHandler(w http.ResponseWriter, r *http.Request) {
+	peter := User{
+		Firstname: "John",
+		Lastname:  "Doe",
+		Age:       25,
+	}
+
+	json.NewEncoder(w).Encode(peter) // peterの情報をJSONにencode
+}
+
 func main() {
-	http.HandleFunc("/decode", func(w http.ResponseWriter, r *http.Request) {
-		var user User                         // structで定義されたUser型の変数user
-		json.NewDecoder(r.Body).Decode(&user) // JSONデータを読み込み、decodeして構造体Userのuserのアドレスに格納
-
-		fmt.Fprintf(w, "%s %s is %d years old!", user.Firstname, user.Lastname, user.Age) // %sは文字列、%dは整数
-	})
-
-	http.HandleFunc("/encode", func(w http.ResponseWriter, r *http.Request) {
-		peter := User{
-			Firstname: "John",
-			Lastname:  "Doe",
-			Age:       25,
-		}
-
-		json.NewEncoder(w).Encode(peter) // peterの情報をJSONにencode
-	})
+	http.HandleFunc("/encode", encodeHandler)
+	http.HandleFunc("/decode", decodeHandler)
 
 	http.ListenAndServe(":8080", nil)
 }
