6f72991bf58047a8c7121e839a375312388f3de9
Keita Sakurai <keitasakuraims@gmail.com>
2023-07-24 20:19:06 +0900

    changed functions

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
@@ -11,24 +12,23 @@ type User struct {
 	Age       int    `json:"age"`
 }
 
-func encodeHandler(w http.ResponseWriter, r *http.Request){
-	peter := User{
-		Firstname: "John",
-		Lastname:  "Doe",
-		Age:	   25,
-	}
-
-	json.NewEncoder(w).Encode(peter)  // peterの情報をJSONにencode
-}
-
-func decodeHandler(w http.ResponseWriter, r *http.Request){
-	var 
-}
-
-
 func main() {
-	http.HandleFunc("/encode", encodeHandler)
-	http.HandleFunc("/decode", decodeHandler)
+	http.HandleFunc("/decode", func(w http.ResponseWriter, r *http.Request) {
+		var user User                         // structで定義されたUser型の変数user
+		json.NewDecoder(r.Body).Decode(&user) // JSONデータを読み込み、decodeして構造体Userのuserのアドレスに格納
+
+		fmt.Fprintf(w, "%s %s is %d years old!", user.Firstname, user.Lastname, user.Age) // %sは文字列、%dは整数
+	})
+
+	http.HandleFunc("/encode", func(w http.ResponseWriter, r *http.Request) {
+		peter := User{
+			Firstname: "John",
+			Lastname:  "Doe",
+			Age:       25,
+		}
+
+		json.NewEncoder(w).Encode(peter) // peterの情報をJSONにencode
+	})
 
 	http.ListenAndServe(":8080", nil)
 }
